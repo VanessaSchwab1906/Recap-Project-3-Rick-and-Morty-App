18bc02474db9e6e77cda996687ffb7233dc99b28
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-04-06 11:54:04 +0200

    Fetch data function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log("hello");
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,28 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+// Function to fetch characters from an API and create HTML cards for each character
+const fetchCharacters = async () => {
+  //try {
+  // Fetch data from API
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await response.json();
+  console.log(data);
+  // Import createCharacterCard function
+  //const { createCharacterCard } = await import("./createCharacterCard");
+
+  // Empty cardContainer
+  //const cardContainer = document.querySelector("#cardContainer");
+  //cardContainer.innerHTML = "";
+
+  // Create HTML card for each character and append it to the cardContainer
+  //data.forEach((character) => {
+  //const card = createCharacterCard(character);
+  //cardContainer.appendChild(card);
+  /*  };
+  } catch (err) {
+    console.error(err);
+  } */
+};
+fetchCharacters();
